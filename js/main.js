cfa49de356588009e406f1df2ea0c6b13d65e5b2
Alexis Baltazar <alexisbaltazar@Alexiss-MacBook-Pro.local>
2019-02-10 00:43:50 -0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+
+var fieldInp = document.querySelector('#inp');
+var plus = document.querySelector('#add');
+var minus = document.querySelector('#sub');
+var result = document.querySelector('#res');
+
+fieldInp.value = 1;
+
+
+plus.addEventListener('click', addNum);
+minus.addEventListener('click', subtractNum);
+
+function addNum() {
+
+    for (var i = 0; i < fieldInp.value; i++) {
+        result.textContent = parseInt(result.textContent) + 1;
+    }
+}
+
+function subtractNum() {
+    for (var i = 0; i < fieldInp.value; i++) {
+        result.textContent = parseInt(result.textContent) - 1;
+    }
+    if (parseInt(result.textContent) < 0) {
+        result.style.color = 'red';
+    }
+}
+
